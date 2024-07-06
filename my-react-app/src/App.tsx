d72c504503d4cfabc4488b89b9f4d9864fea984a
pirnataly <piroznatal@gmail.com>
2024-07-06 19:07:08 +0300

    fix: change settings of husky

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -9,8 +9,8 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank"></a>
-        <a href="https://react.dev" target="_blank"
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLgo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
