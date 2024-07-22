aadec73f661d1ff857e1aa147ad46ea53ab8cb99
pirnataly <piroznatal@gmail.com>
2024-07-22 19:52:18 +0300

    feat: add Cards as a functional component

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,27 +1,18 @@
 import React from 'react';
-import { CardProps, Photo } from '../interfaces/types';
+import { CardProps } from '../interfaces/types';
 
-export default class Cards extends React.Component<CardProps, unknown> {
-  static photos: Photo[] | [];
-
-  constructor(props: CardProps) {
-    super(props);
-    Cards.photos = props.photos;
-  }
-
-  render() {
-    const { headingText } = this.props;
-    return (
-      <div className="cards-container">
-        {Cards.photos.map((post) => (
-          <div className="results-card" key={post.id}>
-            <div className="results-card__image">
-              <img src={post.url_l} className="results-card__image" alt={headingText ?? 'photo'} />
-            </div>
-            <h5 className="card-description">{post.title}</h5>
+export default function Cards(props: CardProps) {
+  const { photos, headingText } = props;
+  return (
+    <div className="cards-container">
+      {photos.map((post) => (
+        <div className="results-card" key={post.id}>
+          <div className="results-card__image">
+            <img src={post.url_l} className="results-card__image" alt={headingText ?? 'photo'} />
           </div>
-        ))}
-      </div>
-    );
-  }
+          <h5 className="card-description">{post.title}</h5>
+        </div>
+      ))}
+    </div>
+  );
 }
